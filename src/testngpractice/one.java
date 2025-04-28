92429491744d08d49e7a9f34fdfa668df0abe9cc
Hanisha Shivnani <hanishashivnani1234@gmail.com>
2025-04-28 17:49:01 +0530

    Your commit message

--- src/testngpractice/one.java
+++ src/testngpractice/one.java
@@ -13,14 +13,11 @@ public class one {
 		System.out.println("Good Morning");
 	}
 	
-<<<<<<< HEAD
-=======
 	@Test
 	public void third() {
 		System.out.println("See You Soon!!");
 	}
->>>>>>> c4db70e90093f1f760d51cc96337088143386a22
-	
+
 	@Test
 	public void forth() {
 		System.out.println("Good Bie");
